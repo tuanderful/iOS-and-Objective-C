f3282abd7fe91252a2e510ce9bd0beab888ed9bf
Tuan Huynh <tuanlh@gmail.com>
2014-02-14 09:26:14 -0800

    CarValet: implement init, accessor methods

--- Learning-iOS-Development/CarValet/CarValet/Car.m
+++ Learning-iOS-Development/CarValet/CarValet/Car.m
@@ -16,13 +16,74 @@
 }
 
 // first thing to implement is init
-- (id)init {
+- (id)init
+{
     self = [super init];        // Always initialize the superclass first.
     if (self != nil) {
         _year = 1900;
         _fuelAmount = 0.0f;
     }
     return self;
+}
+
+// "Base Initializer" pattern
+- (id)initWithMake: (NSString *)make
+             model: (NSString *)model
+              year: (int)year
+        fuelAmount: (float)fuelAmount
+{
+    
+    self = [super init];
+
+    if (self != nil) {
+        _make= make;
+        _model = model;
+        _year = year;
+        _fuelAmount = fuelAmount;
+    }
+    
+    return self;
+}
+
+- (void)printCarInfo
+{
+    // Note that we don't check for year or fuelAmount. When we
+    //  initialize, either we do a full initialization [initWithMake]
+    //  or we call the [init], which defaults the year and fuel amount.
+    if(!_make) return;
+    if(!_model) return;
+    
+    NSLog(@"Car Make: %@", _make);                                  // NSLog is wrapper around printf
+    NSLog(@"Car Model: %@", _model);                                // Print Objects: %@
+    NSLog(@"Car Year: %d", _year);                                  // Log Integer: %d
+    NSLog(@"Number of Gallons in Tank: %0.2f", _fuelAmount);        // Log & Format Float: %0.2f
+}
+
+// Accessor Methods
+- (float)fuelAmount
+{
+    return _fuelAmount;
+}
+
+- (int)year
+{
+    return _year;
+}
+
+- (NSString*)make
+{
+    return [_make copy];          // returning objects: make a copy
+}
+
+- (NSString*)model
+{
+    return [_model copy];
+}
+
+
+- (void)setFuelAmount: (float)fuelAmount
+{
+    _fuelAmount = fuelAmount;
 }
 
 @end
